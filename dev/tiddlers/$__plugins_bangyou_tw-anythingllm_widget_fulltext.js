57fc6d17e448aa7d919bab90e1f83c8aa2dd7b8f
Bangyou Zheng <bangyou.zheng@csiro.au>
2024-08-19 13:59:50 +1000

    Add fulltext

--- dev/tiddlers/$__plugins_bangyou_tw-anythingllm_widget_fulltext.js
+++ dev/tiddlers/$__plugins_bangyou_tw-anythingllm_widget_fulltext.js
@@ -41,16 +41,64 @@ Anything LLM in tiddlywiki 5
             dom_container.id = "tw-anything-llm-workspace";
 
             parent.insertBefore(dom_container, nextSibling);
-            
             async function load_fulltext(path) {
                 const response = await fetch(path);
                 if (!response.ok) {
-                    throw new Error(`HTTP error! status: ${response.status}`);
+                    let ele = document.createElement("p");
+                    ele.innerText = "No full text is found.";
+                    dom_container.appendChild(ele);
+                    return;
                 }
                 const data = await response.text();
-                let html_text = $tw.wiki.renderText("text/html", "text/vnd.tiddlywiki", data);
+                let txt = data.split(/\r?\n/);
+                let h2_keywords = [
+                    "abstract",
+                    "introduction", 
+                    "material",
+                    "method",
+                    "result",
+                    "discussion",
+                    "conclusion"
+                ];
+                // hide all elements after this keywords are dectected
+                let hidden_keywords = [
+                    "appendix",
+                    "supplementary", 
+                    "declaration",
+                    "acknowledgements"
+                ];
+                let hidden = false;
+                for (let i = 0; i < txt.length; i++) {
+                    let txt_i = txt[i].trim();
+                    if (txt_i === "") {
+                        continue;
+                    }
+                    let word_count = txt_i.split(/\s+/).length;
+                    let ele;
+                    if (word_count < 10) {
+                        // for h2 heading
+                        let tag = "h3";
+                        if (h2_keywords.some(el => txt_i.toLowerCase().includes(el))) {
+                            tag = "h2";
+                        }
+                        ele = document.createElement(tag);
+                        // hide some elements
+                        if (hidden || hidden_keywords.some(el => txt_i.toLowerCase().includes(el))) {
+                            hidden = true;
+                        }
+                        ele.innerText = txt_i;
+                    } else {
+                        ele = document.createElement('p');
+                        ele.innerHTML = $tw.wiki.renderText("text/html", "text/vnd.tiddlywiki", txt_i);
+                    }
+                    if (ele !== undefined) {
+                        ele.hidden = hidden;
+                        dom_container.appendChild(ele);
+                    }
+                }
+                //let html_text = $tw.wiki.renderText("text/html", "text/vnd.tiddlywiki", data);
                 
-                dom_container.innerHTML = html_text ;  
+                //dom_container.innerHTML = html_text ;  
             }
             load_fulltext(path);
             
